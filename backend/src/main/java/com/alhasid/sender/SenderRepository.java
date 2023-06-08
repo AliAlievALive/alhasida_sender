d77f02047b5a8bd370dffbdba32af2ebd61846af
almir <madarat87@gmail.com>
2023-06-08 19:41:05 +0300

    fixed some errors

--- backend/src/main/java/com/alhasid/sender/SenderRepository.java
+++ backend/src/main/java/com/alhasid/sender/SenderRepository.java
@@ -1,7 +1,9 @@
 package com.alhasid.sender;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SenderRepository extends JpaRepository<Sender, Long> {
     boolean existsSenderByEmail(String email);
 }
