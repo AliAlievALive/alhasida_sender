9d743cbfc53ac0d4535014b05427b75bcbb8797a
almir <madarat87@gmail.com>
2023-06-12 19:47:24 +0300

    added dashboard

--- frontend/react_frompast/src/App.jsx
+++ frontend/react_frompast/src/App.jsx
@@ -1,53 +1,21 @@
-import UserProfile from "./UserProfile.jsx";
-import {useEffect, useState} from "react";
+import SidebarWithHeader from "./shared/SideBar.jsx";
+import {Button} from "@chakra-ui/react";
+import {useEffect} from "react";
+import {getSenders} from "./services/client.js";
 
-const users = [
-    {name: "Ali", age: 22, gender: "Male"},
-    {name: "Jamila", age: 44, gender: "Female"},
-    {name: "Alex", age: 22, gender: "Male"},
-    {name: "Alim", age: 30, gender: "Male"},
-    {name: "Anna", age: 18, gender: "Female"},
-]
+const App = () => {
 
-const UserProfiles = ({users}) => (
-    <div>
-        {users.map((user, index) => (
-            <UserProfile
-                key={index}
-                name={user.name}
-                age={user.age}
-                gender={user.gender}
-                imageNumber={index}
-            />
-        ))}
-    </div>
-)
-
-function App() {
-
-    const [counter, setCounter] = useState(0);
-    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
-        setIsLoading(true);
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 4000);
-
-        return () =>
-            console.log("cleanup functions")
-    }, [])
-
-    if (isLoading) {
-        return "loading...";
-    }
+        getSenders().then(res => {
+            console.log(res)
+        }).catch(err => console.log(err));
+    }, []);
 
     return (
-        <div>
-            <button onClick={() => setCounter(prevCounter => prevCounter + 1)}>Increment counter</button>
-            <h1>{counter}</h1>
-            <UserProfiles users={users}/>
-        </div>
-    );
-}
+        <SidebarWithHeader>
+            <Button colorScheme='teal' variant='outline'>Click me</Button>
+        </SidebarWithHeader>
+    )
+};
 
 export default App
