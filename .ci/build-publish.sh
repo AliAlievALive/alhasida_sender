6b582b32622d57e83d77f148a78a7263c5dc347c
almir <madarat87@gmail.com>
2023-06-19 04:14:07 +0300

    fixed like in amigoscode

--- .ci/build-publish.sh
+++ .ci/build-publish.sh
@@ -1,12 +1,13 @@
-set -e
-
 : "${USERNAME:?USERNAME not set or empty}"
 : "${REPO:?REPO not set or empty}"
 : "${TAG:?TAG not set or empty}"
 
-
-echo "Building Docker image..."
 docker buildx create --use
 
-echo "Building Docker image using buildx..."
-docker buildx build --platform=linux/amd64, linux/arm64 -t "${USERNAME}/${REPO}:${TAG}" -t "${USERNAME}/${REPO}:latest" "${@:2}" --push "$1"
+docker buildx build \
+    --platform=linux/amd64,linux/arm64 \
+    -t "${USERNAME}/${REPO}:${TAG}" \
+    -t "${USERNAME}/${REPO}:latest" \
+    "${@:2}" \
+    --push \
+    "$1"
