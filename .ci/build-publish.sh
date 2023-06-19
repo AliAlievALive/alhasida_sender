b378e628a07f59b4058224494155839043e8a4e4
almir <madarat87@gmail.com>
2023-06-19 03:59:40 +0300

    fixed .sh and CD script

--- .ci/build-publish.sh
+++ .ci/build-publish.sh
@@ -1,7 +1,12 @@
+set -e
+
 : "${USERNAME:?USERNAME not set or empty}"
 : "${REPO:?REPO not set or empty}"
 : "${TAG:?TAG not set or empty}"
 
+
+echo "Building Docker image..."
 docker buildx create --use
 
+echo "Building Docker image using buildx..."
 docker buildx build --platform=linux/amd64, linux/arm64 -t "${USERNAME}/${REPO}:${TAG}" -t "${USERNAME}/${REPO}:latest" "${@:2}" --push "$1"
